c38bad304ae489c5810bd91045abffc9479d20fb
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-02-04 01:05:02 +0300

    added: EncryptionModel

---
+++ src/main/model/EncryptionModel.java
@@ -0,0 +1,15 @@
+package main.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.nio.file.Path;
+@Getter
+@Setter
+@AllArgsConstructor
+public class EncryptionModel {
+    private Path source;
+    private Path destination;
+    private int key;
+}
