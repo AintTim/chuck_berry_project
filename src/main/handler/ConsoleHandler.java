fe98c47d743698053d2d3ba3129dc8d5d5d11fd7
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-02-04 22:53:32 +0300

    + selectOption(), presentAvailableOptions()

--- src/main/handler/ConsoleHandler.java
+++ src/main/handler/ConsoleHandler.java
@@ -1,8 +1,12 @@
 package main.handler;
 
+import static main.constant.Instruction.*;
 import main.model.EncryptionModel;
+import main.model.Option;
 
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class ConsoleHandler {
@@ -12,15 +16,49 @@ public class ConsoleHandler {
             this.scanner = scanner;
         }
 
-        public EncryptionModel createEncryptedObject() {
-            System.out.println("Введите адрес файла оригинального текста: ");
-            Path source = Path.of(scanner.nextLine());
+        public EncryptionModel createEncryptedObject(boolean hasReference) {
+            Path source = Path.of(provideData(ENTER_SOURCE_PATH));
+            Path destination = Path.of(provideData(ENTER_DESTINATION_PATH));
+            Path reference = null;
+            int key;
+            if (hasReference) {
+                reference = Path.of(provideData(ENTER_REFERENCE_PATH));
+                key = 1;
+            } else {
+                key = Integer.parseInt(provideData(ENTER_KEY));
+                while (!validateKey(key)) {
+                    key = Integer.parseInt(provideData(INVALID_KEY));
+                }
+            }
 
-            System.out.println("Введите адрес файла с результатом: ");
-            Path destination = Path.of(scanner.nextLine());
+            return new EncryptionModel(source, destination, reference, key);
+        }
 
-            System.out.println("Введите ключ шифрования: ");
-            int key = scanner.nextInt();
-            return new EncryptionModel(source, destination, key);
+        public void presentAvailableOptions() {
+            System.out.println(AVAILABLE_OPTIONS);
+            Arrays.stream(Option.values()).forEach(o -> {
+                System.out.printf("Для выбора опции '%s' нажмите %d\t(%s)%n", o.name(), o.getValue(), o.getDescription());
+            });
+        }
+
+        public Option selectOption() {
+            System.out.println(SELECT_OPTION);
+            int input = Integer.parseInt(scanner.nextLine());
+            Optional<Option> option = Option.getFromValue(input);
+            while (option.isEmpty()) {
+                System.out.println(INVALID_OPTION);
+                input = Integer.parseInt(scanner.nextLine());
+                option = Option.getFromValue(input);
+            }
+            return option.get();
+        }
+
+        private String provideData(String instruction) {
+            System.out.println(instruction);
+            return scanner.nextLine();
+        }
+
+        private static boolean validateKey(int key) {
+            return key > 0;
         }
 }
