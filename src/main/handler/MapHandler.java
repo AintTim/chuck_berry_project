cc43b3a9c9354f8727e7319846084225762b4845
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-02-05 10:03:41 +0300

    + getSortedValues()

--- src/main/handler/MapHandler.java
+++ src/main/handler/MapHandler.java
@@ -1,5 +1,8 @@
 package main.handler;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 
 public class MapHandler {
@@ -8,7 +11,15 @@ public class MapHandler {
 
     }
 
-    public static <K, V> K getKey(Map< K, V> map, V value) {
+    public static <K, V> List<K> getSortedValues(Map<K, V> map, Comparator<Map.Entry<K, V>> comparator) {
+        return map.entrySet().stream()
+                .sorted(comparator.reversed())
+                .collect(ArrayList::new,
+                        (list, entry) -> list.add(entry.getKey()),
+                        ArrayList::addAll);
+    }
+
+    public static <K, V> K getKey(Map<K, V> map, V value) {
         return map.entrySet()
                 .stream()
                 .filter(e -> e.getValue().equals(value))
