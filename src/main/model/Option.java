c78715466da05bb1b7c7e94f2334126546af2f9b
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-02-05 10:03:19 +0300

    + option

--- src/main/model/Option.java
+++ src/main/model/Option.java
@@ -1,4 +1,28 @@
 package main.model;
+import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.Optional;
+
+import static main.constant.Description.*;
+@Getter
 public enum Option {
+    ENCRYPT(1, ENCRYPT_DESC),
+    DECRYPT(2, DECRYPT_DESC),
+    BRUTE_FORCE(3, BRUTE_FORCE_DESC),
+    STATISTICAL_ANALYSIS(4, STATISTICS_ANALYSIS_DESC);
+
+    private final int value;
+    private final String description;
+
+    Option(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public static Optional<Option> getFromValue(int value) {
+        return Arrays.stream(Option.values())
+                .filter(o -> o.getValue() == value)
+                .findFirst();
+    }
 }
