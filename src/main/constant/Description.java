f73951c4f3a3c6a933f83f7b157facc01a4649dd
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-02-05 10:02:18 +0300

    + constant classes

---
+++ src/main/constant/Description.java
@@ -0,0 +1,9 @@
+package main.constant;
+
+public class Description {
+    public static String ENCRYPT_DESC = "Шифровка текста";
+    public static String DECRYPT_DESC = "Расшифровка текста с помощью ключа";
+    public static String BRUTE_FORCE_DESC = "Расшифровка текста с помощью brute force";
+    public static String STATISTICS_ANALYSIS_DESC = "Расшифровка текста с помощью статистического анализа";
+
+}
---
+++ src/main/constant/Instruction.java
@@ -0,0 +1,21 @@
+package main.constant;
+
+public class Instruction {
+
+    private Instruction() {}
+    public static final String ENTER_SOURCE_PATH = "(Источник) Введите адрес файла: ";
+    public static final String ENTER_DESTINATION_PATH = "(Результат) Введите адрес файла: ";
+    public static final String ENTER_REFERENCE_PATH = "(Пример) Введите адрес файла: ";
+    public static final String ENTER_KEY = "Введите ключ шифрования: ";
+    public static final String CHOOSE_ALPHABET = "Выберите используемый алфавит из представленных: ";
+    public static final String INVALID_KEY = "Введенный ключ шифрования должен быть больше нуля.\nПожалуйста, введите новый ключ: ";
+    public static final String AVAILABLE_OPTIONS = "Список доступных опций: ";
+    public static final String SELECT_OPTION = "Выберите опцию: ";
+    public static final String INVALID_OPTION = "Выбранная опция отсутствует в списке доступных.\nПожалуйста, выберите существующую опцию: ";
+    public static final String BRUTE_FORCE_VALID_KEY = "Файл был зашифрован с помощью ключа - ";
+
+
+
+
+
+}
