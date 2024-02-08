188511e53c13ca8f712cdd0c94875d7afaa64245
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-02-08 13:05:51 +0300

    changed: main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,39 +1,34 @@
 package main;
 
-import static main.model.Alphabet.*;
+import static main.constant.Instruction.ENCRYPTION_KEY;
 
 import main.handler.ConsoleHandler;
+import main.handler.FileContentHandler;
 import main.handler.FileHandler;
 import main.handler.FileValidator;
+import main.model.Alphabet;
 import main.model.Option;
 
-import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
-    private static final Path SOURCE_FILE_PATH = Path.of("src/resources/source.txt");
-    private static final Path DEST_FILE_PATH = Path.of("src/resources/encrypted.txt");
-    private static final Path DEST_FILE_PATH_1 = Path.of("src/resources/decrypted.txt");
 
     public static void main(String[] args) {
         FileHandler fileHandler = new FileHandler(new FileValidator());
+        FileContentHandler fileContentHandler = new FileContentHandler(fileHandler);
         ConsoleHandler console = new ConsoleHandler(new Scanner(System.in));
-        CryptoAnalyzer analyzer = new CryptoAnalyzer(fileHandler, RUSSIAN, PUNCTUATION);
 
-        console.presentAvailableOptions();
+        console.presentAlphabets();
+        CryptoAnalyzer analyzer = new CryptoAnalyzer(fileContentHandler, console.selectAlphabet().toArray(new Alphabet[0]));
+
+        console.presentOptions();
         Option option = console.selectOption();
 
         switch (option) {
             case ENCRYPT -> analyzer.encrypt(console.createEncryptedObject(false));
             case DECRYPT -> analyzer.decrypt(console.createEncryptedObject(false));
-            case BRUTE_FORCE -> analyzer.bruteForce(console.createEncryptedObject(true));
-            case STATISTICAL_ANALYSIS -> analyzer.bruteForce(null);
+            case BRUTE_FORCE -> console.announce(ENCRYPTION_KEY + analyzer.bruteForce(console.createEncryptedObject(true)));
+            case STATISTICAL_ANALYSIS -> console.announce(ENCRYPTION_KEY + analyzer.statisticalAnalysis(console.createEncryptedObject(true)));
         }
-
-        //bruteForce - 3 файла: откуда, куда и референс
-        //статистика - 3 файла екст. Текст / екст, текст
-        // список системных файлов ОС
-
-
     }
 }
