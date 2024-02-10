4b4786e6bb89ec9fb40b092899d803d0f16898ea
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-02-10 13:48:26 +0300

    added: javadoc

--- src/main/handler/FileContentHandler.java
+++ src/main/handler/FileContentHandler.java
@@ -24,11 +24,26 @@ public class FileContentHandler {
         fileHandler.writeFile(destination, content);
     }
 
+    /**
+     * Подсчитываем частоту для всех уникальных слов в файле (ключи) и возвращаем некоторое их количество
+     * @see FileContentHandler#collectUniqueWords(Path, String)
+     * @param source путь файла-источника
+     * @param splitter символ, разделяющий слова
+     * @param limit количество возвращаемых ключей
+     * @return Список наиболее распространённых слов в файле
+     */
     public List<String> collectCommonWords(Path source, String splitter, int limit) {
         Map<String, Integer> dictionary = collectUniqueWords(source, splitter);
         return MapHandler.getSortedValues(dictionary, Comparator.comparingInt(Map.Entry::getValue))
                 .subList(0, limit);
     }
+
+    /**
+     * @see main.model.Alphabet енам с символами
+     * @param source путь файла-источника
+     * @param alphabet список символов для анализа
+     * @return карту с символами и их частотой в рамках файла-источника
+     */
     public Map<Character, Double> collectSymbolFrequency(Path source, Map<Integer, Character> alphabet) {
         Map<Character, MutableInt> dictionary = new HashMap<>();
         for (String line : readContent(source)) {
@@ -56,19 +71,25 @@ public class FileContentHandler {
     }
 
     public Map<String, Integer> collectUniqueWords(Path source, String splitter) {
-        Map<String, MutableInt> dictionary = new HashMap<>();
+        Map<String, Integer> dictionary = new HashMap<>();
         for (String line : readContent(source)) {
-            Arrays.stream(line.split(splitter))
-                    .filter(w -> !w.isBlank())
-                    .forEach(word -> {
-                        MutableInt value = dictionary.get(word);
-                        if (value == null) {
-                            dictionary.put(word, new MutableInt());
-                        } else {
-                            value.increment();
-                        }
-                    });
+            collectUniqueWords(line, splitter).forEach((k,v) -> dictionary.merge(k, 1, Integer::sum));
         }
+        return dictionary;
+    }
+
+    public Map<String, Integer> collectUniqueWords(String source, String splitter) {
+        Map<String, MutableInt> dictionary = new HashMap<>();
+        Arrays.stream(source.split(splitter))
+                .filter(w -> !w.isBlank())
+                .forEach(word -> {
+                    MutableInt value = dictionary.get(word);
+                    if (value == null) {
+                        dictionary.put(word, new MutableInt());
+                    } else {
+                        value.increment();
+                    }
+                });
         return dictionary.entrySet()
                 .stream()
                 .collect(toMap(Map.Entry::getKey, e -> e.getValue().value));
